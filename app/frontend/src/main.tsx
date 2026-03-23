b1ce42b949e7c190c5bda1375c55072408714caa
Edgar Flores <e.flores30nov@gmail.com>
2026-03-22 20:12:49 -0600

    fix google client

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Toaster } from './components/ui/sonner'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Toaster } from "./components/ui/sonner";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // Usar variable de entorno de Vercel/Vite para el ID de Google
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "104169727409-5shv572iv08n6k8aovmivj0iv2d9526n.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID =
+  import.meta.env.VITE_GOOGLE_CLIENT_ID ||
+  "433426856774-t1e521993r6fs4ug2f3oi76o0rtia64n.apps.googleusercontent.com";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider 
-      clientId={GOOGLE_CLIENT_ID}
-      useFedCM={true}
-    >
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID} useFedCM={true}>
       <App />
       <Toaster position="top-right" richColors closeButton />
     </GoogleOAuthProvider>
   </React.StrictMode>,
-)
+);
