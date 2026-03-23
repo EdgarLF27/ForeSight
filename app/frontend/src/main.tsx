fca201768470fda4fd505a8a7d8886ff96461736
Edgar Flores <e.flores30nov@gmail.com>
2026-03-23 00:07:03 -0600

    :

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -6,7 +6,9 @@ import { Toaster } from "./components/ui/sonner";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // Usar variable de entorno de Vercel/Vite para el ID de Google
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_ID =
+  import.meta.env.VITE_GOOGLE_CLIENT_ID ||
+  "433426856774-t1e521993r6fs4ug2f3oi76o0rtia64n.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
